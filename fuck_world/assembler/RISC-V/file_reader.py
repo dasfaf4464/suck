84169c8fde62435789daaf20e0ec2b0803388e0e
dasfaf4464 <richchc@gmail.com>
2026-04-29 23:42:14 +0900

    adjust file reader and add test case

--- fuck_world/assembler/RISC-V/file_reader.py
+++ fuck_world/assembler/RISC-V/file_reader.py
@@ -1,29 +1,40 @@
-
 class FileReader:
     def __init__(self):
-        self.lines = []
+        self.raw_lines = []
+        self.processed_lines = []
 
-    def get_file(self, file_path):
+    def read(self, file_path):
+        self.read_file(file_path)
+        self.preprocess()
+
+        return self.processed_lines
+
+    def read_file(self, file_path):
         try:
             with open(file_path, "r", encoding="utf-8") as f:
-                self.lines = f.readlines()
+                self.raw_lines = f.readlines()
         except FileNotFoundError:
             print(f"에러: {file_path} 파일을 찾을 수 없습니다.")
-        return self.lines
 
     def preprocess(self):
-        processed = []
-        for line in self.lines:
-            clean_line = line.split("#")[0].strip()
-
-            if clean_line:
-                processed.append(clean_line)
-
-        self.lines = processed
+        """
+        remove comment(#) line
+        remove carriage return(\r)
+        remove space(\t, ' ')
+        """
+        lines = self.raw_lines
+        for line in lines:
+            comment = line.split("#")[0]
+            carriage = comment.replace("\r", "").replace("\\r", "")
+            space = carriage.strip(" ")
+            tab = space.strip("\t")
+            if tab and tab != "\n":
+                self.processed_lines.append(tab)
 
 
 if __name__ == "__main__":
     fr = FileReader()
-    fr.get_file(r"C:\Data\suck\test\agent.txt")
+    fr.read_file(r"C:\Data\suck\test\preprocess_test.txt")
     fr.preprocess()
-    print(fr.lines)
+    print(fr.raw_lines)
+    print(fr.processed_lines)
